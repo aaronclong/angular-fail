22aec828170af448bffd27debd740a9b70fffff0
Aaron Long <aaron.long.c@gmail.com>
2017-06-02 14:53:04 -0400

    Problems

---
+++ src/pages/map/map.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'map',
+  templateUrl: 'map.html'
+})
+export class MapPage {
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  }
+
+}
